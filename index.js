d26387a895adb8b35a4f79039c3afd32a10431a7
elvis <elvis.adomnica@gmail.com>
2018-03-04 12:58:38 +0100

    Clean up docs and code

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 var wikiToPhilosophy = require('./lib/wiki-to-philosophy');
-var readlineSync = require('readline-sync');
 
-// Wait for user's response.
-//var answer = readlineSync.question('What Wikipedia page do you want to test? ');
 wikiToPhilosophy
   .start(process.argv[2])
   .then(path => {
-    console.log('------------------------------------');
     console.log(path);
-    console.log('------------------------------------');
   })
   .catch(err => {
-    console.log('------------------------------------');
     console.log(err);
-    console.log('------------------------------------');
   });
