5b63b11a09455ea3fa322b3b04a1f242398a4870
elvis <elvis.adomnica@gmail.com>
2018-03-04 12:33:10 +0100

    Use promise and add ES6 syntax

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ var readlineSync = require('readline-sync');
 
 // Wait for user's response.
 //var answer = readlineSync.question('What Wikipedia page do you want to test? ');
-wikiToPhilosophy.start(
-  process.argv[2],
-  function(pageName) {
-    console.log(pageName);
-  },
-  function(err, pathToPhilosophy) {
-    if (err) {
-      console.error(err);
-    }
-    console.log(pathToPhilosophy);
-  }
-);
+wikiToPhilosophy
+  .start(process.argv[2])
+  .then(path => {
+    console.log('------------------------------------');
+    console.log(path);
+    console.log('------------------------------------');
+  })
+  .catch(err => {
+    console.log('------------------------------------');
+    console.log(err);
+    console.log('------------------------------------');
+  });
